a192da1fea50e508d747d0a73f8a208fd728eba2
dileep vasa <>
2019-02-10 17:48:19 -0600

    Created two Targets & Config changes

---
+++ XfinityCodingTest/Simpsons/Config.swift
@@ -0,0 +1,27 @@
+//
+//  Config.swift
+//  Simpsons Character Viewer
+//
+//  Created by Dileep Vasa on 2/10/19.
+//  Copyright © 2019 Xfinity. All rights reserved.
+//
+
+import Foundation
+
+class Config {
+    
+    static var endpoint: String  {
+        get {
+            
+            if let path = Bundle.main.path(forResource: "Info", ofType: "plist") {
+                if let dic = NSDictionary(contentsOfFile: path) {
+                    return dic["ENDPOINT_URL"] as? String ?? "couldn't retrieve endpoint from plist file"
+                }
+            }
+            
+            return "couldn't retrieve endpoint from plist file"
+        }
+    }
+}
+
+
